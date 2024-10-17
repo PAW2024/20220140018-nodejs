fd9726e46697e3e1f34968dc11e45788ac627e76
ijaamm <ijammusyaf@gmail.com>
2024-10-17 12:51:14 +0700

    POST API

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express'); // Mengimport express
 const app = express();
+const todoRoutes = require('./routes/todo.js');
 const port = 3000;
+
+app.use(express.json());
+app.use('/todos',todoRoutes);
 
 // Mengatur view engine sebagai ejs
 app.set('view engine', 'ejs');
